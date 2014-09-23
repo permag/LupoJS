4bd49cef7d6ca0c85c7b9eb95b7d26650f0422c6
Per Magnusson <killingfloor00@gmail.com>
2014-09-23 11:15:08 +0200

    replaced eval

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var ob = {
+    person: {
+        test: function () {
+            return "HEJEHEHE";
+        },
+        value: "hej var",
+        arr: [1, 2, 3, 4],
+        objArr: {hej: [4, 5, 6]}
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
 (function ($) {
 
     function init() {
@@ -8,7 +29,7 @@
         return {
             name: 'Sven Svensson',
             age: 65,
-            phones: ['555-1234', '555-5678', '555-12414'],
+            phones: ['555-1234', '555-5678', '555-9877'],
             houses: {resident: 'Morbacka', summer: 'Hedemora', ob: {test: 'test'}}
         }
     }
@@ -43,6 +64,10 @@
         while (match = re.exec(html)) {
             bracket = match[0];
             member = $.trim(match[1]);
+            memberParam = member.match(/[^[\]]+(?=])/);
+            if (memberParam) {
+                member = member.replace('['+memberParam[0]+']', '');  // remove [x]
+            }
             arr.push(member);
             var loop = member.substr(0, 4);
             if (loop === 'for ') {
@@ -55,10 +80,11 @@
                     
                     if (loopMember === 'endfor') {
                         html = html.replace(loopBracket, '');  // remove endfor tag
-                        var n = eval('$model.'+loopArr[0][1]+'.length');
+   
                         for (var i=0; i<loopArr.length; i+=1) {
                             var modBracket = loopArr[i][0].substring(1, loopArr[i][0].length-1);
-                            var list = eval('$model.'+loopArr[i][1]);
+                            var list = getObjectByName(loopArr[i][1], $model, memberParam);
+
                             for (var y=0; y<list.length; y+=1) {
                                 html += modBracket;
                                 html = html.replace(modBracket, list[y]);
@@ -72,11 +98,34 @@
                     html = html.replace(loopBracket, loopBracket.substring(1, loopBracket.length-1));
                 }
             } else {
-                var fu = new Function('$model', 'return $model.' + member + ';');
-                html = html.replace(bracket, fu($model));
+                var params;
+                if (memberParam) {
+                    params = memberParam;
+                } else {
+                    params = null;
+                }
+                html = html.replace(bracket, getObjectByName(member, $model, memberParam));
+                
             }
         }
         return html;
+    }
+
+    function getObjectByName(objName, context, args) {
+        var args = [].slice.call(arguments).splice(2);
+        var namespaces = objName.split(".");
+        var func = namespaces.pop();
+        var i, n = namespaces.length;
+        for (var i = 0; i < n; i+=1) {
+            context = context[namespaces[i]];
+        }
+        // return context[func].apply(this, args); // for functions
+
+        if (args[0] !== null) {
+            return context[func][args];
+        } else {
+            return context[func]; // for variables
+        }
     }
 
     init();
